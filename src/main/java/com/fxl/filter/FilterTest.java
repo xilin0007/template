40b9f44da32997a5bcd94fd407e6d6106348d39b
xilin0007 <fangxlsmile@163.com>
2022-02-18 18:29:36 +0800

    过滤器测试

--- src/main/java/com/fxl/filter/FilterTest.java
+++ src/main/java/com/fxl/filter/FilterTest.java
@@ -48,8 +48,10 @@ public class FilterTest implements Filter {
 		//response.setHeader("Access-Control-Allow-Origin", origin);
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		//控制浏览器在多长时间内（单位s）无需在请求时发送预检请求，从而减少不必要的预检请求，放出跨域访问，出现两次接口请求（一次options请求，一次get/post请求）
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization");
+		//表示是否允许发送Cookie。默认情况下，Cookie不包括在CORS请求之中。设为true，即表示服务器明确许可，Cookie可以包含在请求中，一起发给服务器
 		response.setHeader("Access-Control-Allow-Credentials", "true");*/
 		chain.doFilter(request, response); // 让目标资源执行，放行
 		System.out.println("FilterTest执行后！！！");
